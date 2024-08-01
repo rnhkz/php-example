ef84191d513a55e21976d202298fcd69539eb336
rnhkz <alb190010@utdallas.edu>
2024-08-01 16:42:02 -0500

    Variables + Boolean

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+        $name = "Dark Matter";
+        $read = false;
+
+        if ($read) {
+            $message = "You have read $name";
+        } else {
+            $message = "You have not read $name";
+        }
+    ?>
+    
+    <h1>
+        <?= $message ?>
+    </h1>
+
+</body>
+
+</html>
