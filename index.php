a981d9b7b7eb7c4afaab7f166019fbe4fe7758ca
rnhkz <alb190010@utdallas.edu>
2024-08-01 17:07:09 -0500

    Arrays & Associative Arrays

--- index.php
+++ index.php
@@ -5,33 +5,35 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 
 <body>
+    <h1>Recommended Books</h1>
+    
     <?php
-        $name = "Dark Matter";
-        $read = false;
-
-        if ($read) {
-            $message = "You have read $name";
-        } else {
-            $message = "You have not read $name";
-        }
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Phillip K. Dick',
+                'purchaseUrl' => "http://example.com"
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'purchaseUrl' => "http://example.com"
+            ],
+        ];
     ?>
     
-    <h1>
-        <?= $message ?>
-    </h1>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 
 </body>
 
